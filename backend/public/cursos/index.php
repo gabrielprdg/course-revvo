3b0a6cc5b03c402efacbe87309e2d8ab64b06243
gabrielprdg <gabriel.pr07@hotmail.com>
2025-07-12 00:23:27 -0300

    fix(backend): adjust apache config and docker volumes for correct public dir routing

--- backend/public/cursos/index.php
+++ backend/public/cursos/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../db.php';
+require_once __DIR__ . '/../../src/db.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
